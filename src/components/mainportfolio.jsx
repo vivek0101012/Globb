63be19af73699ec77bb8639e4a3629d0e933075d
VIVEK NISHAD <vknitk2026@gmail.com>
2025-05-12 19:59:50 +0530

    minor changes in ui

--- src/components/mainportfolio.jsx
+++ src/components/mainportfolio.jsx
@@ -179,7 +179,7 @@ return   <tr className="w-full max-h-[300px] overflow-y-scroll z-10 text-[9px] m
     
     
                     <img 
-    className="w-2 md:w-6 md:h-6 h-2 "
+    className="w-4 md:w-6 md:h-6 h-4 "
       src={data.c > data.pc ? "/images/up.png" : "/images/down.png"} 
       alt="Stock Trend" 
     />
@@ -188,12 +188,12 @@ return   <tr className="w-full max-h-[300px] overflow-y-scroll z-10 text-[9px] m
 
 
 </td>
-<td className="px-4 py-2    border-opacity-40  border-gray-500 border-opacity-4 "> $  {data.c}  <span>x</span> {e.count} </td>
-<td className={`px-4 py-2   border-opacity-40  border-gray-500 border-opacity-4 ${data.d >= 0 ? "text-green-500" : "text-red-500"}`}>
-  {data.d}
+<td className="px-4 py-2    border-opacity-40  border-gray-500 border-opacity-4 "> $  {(data.c).toFixed(2)}  <span>x</span> {(e.count)} </td>
+<td className={`px-6 py-2   border-opacity-40  border-gray-500 border-opacity-4 ${(data.d).toFixed(2) >= 0 ? "text-green-500" : "text-red-500"}`}>
+  {(data.d).toFixed(2)}
 </td>
-<td className={`px-4 py-2  border-opacity-40 border-gray-500 border-opacity-4 ${data.dp >= 0 ? "text-green-500" : "text-red-500"}`}>{data.dp}%</td>
-<td className="px-4 py-2   border-opacity-40  border-gray-500 border-opacity-4 "> $  {data.c*e.count} </td>
+<td className={`px-4 py-2  border-opacity-40 border-gray-500 border-opacity-4 ${data.dp >= 0 ? "text-green-500" : "text-red-500"}`}>{(data.dp).toFixed(2)}%</td>
+<td className="px-4 py-2   border-opacity-40  border-gray-500 border-opacity-4 ">     { "$ "+(data.c*e.count).toFixed(2)} </td>
 </tr>
 
 
